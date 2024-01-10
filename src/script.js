28e07f33b617261221d37065264e7004808f90ee
robinn007 <124763217+robinn007@users.noreply.github.com>
2024-01-10 09:19:27 +0000

    a git assignment is made

---
+++ src/script.js
@@ -0,0 +1,10 @@
+let ans = document.getElementById("root");
+
+ans.style.color = "blue";
+ans.style.backgroundColor = "yellow";
+ans.style.margin = "20px";
+ans.style.padding = "10px";
+ans.style.fontSize = "18px";
+ans.style.fontWeight = "bold";
+ans.style.height = "200px";
+ans.style.width = "300px";
